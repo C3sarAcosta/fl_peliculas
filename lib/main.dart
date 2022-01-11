9105a600de0ffc526f4e4f6950c875fd85a3334c
César Acosta <c3sar.adan@gmail.com>
2022-01-11 09:28:48 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.light()
+          .copyWith(appBarTheme: const AppBarTheme(color: Colors.purple)),
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: Center(
+          child: Container(
+            child: const Text('Hello World'),
+          ),
+        ),
+      ),
+    );
+  }
+}
