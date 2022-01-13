50fb53e8f39d29880b099ef11488063fe7c4d334
César Acosta <c3sar.adan@gmail.com>
2022-01-13 09:41:10 -0700

    Provider y https

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,28 @@
+import 'package:fl_peliculas/providers/movies_provider.dart';
 import 'package:fl_peliculas/screens/details_screen.dart';
 import 'package:fl_peliculas/screens/home_screen.dart';
 //import 'package:fl_peliculas/screens/screens.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(AppState());
+
+class AppState extends StatelessWidget {
+  const AppState({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => MoviesProvider(),
+          lazy: false,
+        )
+      ],
+      child: MyApp(),
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   @override
